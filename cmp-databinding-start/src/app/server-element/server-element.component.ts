ce00501a6a329a30541b2fb64d13432acfa493de
roxioanes <roxioanes11@gmail.com>
2020-01-06 16:15:48 +0100

    Add Life cycle

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
  @Input("srvElement") element: { type: string, name: string, content: string };
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges){
+
+  }
 
   ngOnInit() {
   }
