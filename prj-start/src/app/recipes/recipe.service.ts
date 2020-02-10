2519b52eb106f8863b2524828adea323cb744fb6
roxioanes <roxioanes11@gmail.com>
2020-02-10 16:07:35 +0100

    End services section

--- prj-start/src/app/recipes/recipe.service.ts
+++ prj-start/src/app/recipes/recipe.service.ts
@@ -1,15 +1,37 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
-  recipeSelected =  new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test desc', 'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg'),
-    new Recipe('Another test recipe', 'this is a test desc', 'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg')
+    new Recipe(
+      "A test recipe",
+      "this is a test desc",
+      "https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg",
+    [
+      new Ingredient("Meat", 1),
+      new Ingredient("French Fries", 20)
+    ]),
+    new Recipe(
+      "Another test recipe",
+      "this is a test desc",
+      "https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg",
+    [
+      new Ingredient("Buns", 1),
+      new Ingredient("French Fries", 20)
+    ])
   ];
 
-  getRecipes(){
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingregients: Ingredient[]) {
+    this.slService.addIngredients(ingregients);
   }
 }
--- prj-start/src/app/shopping-list/shopping-list.service.ts
+++ prj-start/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
 import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -12,5 +14,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
